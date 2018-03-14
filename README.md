45970e99d6a4d8115224c7ea5a72395559766425
JustDoIT <lisasievers.ca@gmail.com>
2018-03-14 02:15:57 -0700

    updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## Lisa's page
+3/12/2018
+Added the Profile page and the Experience-Skills pages so that I could pull API data from this site. 
+I wanted to get the API correct and get data as I would for my application - think it out rather than rushing it through. 
+
+I am still working on the API call but will finish it today. 
--- js/Lisa.js
+++ js/Lisa.js
@@ -1,20 +1,38 @@
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5a7dd2a94b401e45400cd21f/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// TawkTo Chat App
+//
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/5a7dd2a94b401e45400cd21f/default';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+        // button for top of site
+window.onscroll = function() {scrollFunction()};
 
-
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-        alert("Please provide a valid email");
-        return false;
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
     }
 }
-</script>
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// Form
+
+// function validateForm() {
+//     var x = document.forms["myForm"]["fname"].value;
+//     if (x == "") {
+//         alert("Please provide a valid email");
+//         return false;
+//     }
+// }
+// </script>
